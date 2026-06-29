54e1636b5299ee13dcdd9b8dbab3b96b3e8153c0
marufhasan34 <YOUR_GITHUB_EMAIL>
2026-06-29 22:27:13 +0600

    implement JWT for owner add property page

--- src/lib/actions/property.js
+++ src/lib/actions/property.js
@@ -1,14 +1,25 @@
 'use server'
+import { headers } from "next/headers";
+import { auth } from "../auth";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
 export const createProperty = async (newPropertyData) => {
+    const { token } = await auth.api.getToken({
+        headers: await headers()
+    });
+    
+    console.log('baseUrl:', baseUrl)  
+    console.log('token:', token)
+
     const res = await fetch(`${baseUrl}/api/property`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
         },
         body: JSON.stringify(newPropertyData)
-    })
+    });
 
-    return res.json()
+    return res.json();
 }
